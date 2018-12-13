1a5db64aedf8d2208b36d1b051e86dc0f358574d
Pedro Queiroga <psq@cin.ufpe.br>
2018-12-13 20:22:45 -0300

    BestMove.hs

---
+++ src/BestMove.hs
@@ -0,0 +1,13 @@
+module BestMove (bestMove) where
+
+import Board
+import EvaluateBoard
+
+import Data.Maybe (fromJust)
+import Data.List (findIndex)
+
+bestMove :: [Board] -> Color -> Board
+bestMove [] _ = initialBoard
+bestMove bs c = bs !! (fromJust $ findIndex ((==) (foldr1 max evalBs)) evalBs)
+  where evalBs | c == White = map evaluateBoard bs
+               | otherwise = map ((*(-1)) . (evaluateBoard)) bs
