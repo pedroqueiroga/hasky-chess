3eeefda232b01aa3c3ef0624d406cc6fbb5f5b52
Pedro Queiroga <psq@cin.ufpe.br>
2018-12-14 19:05:31 -0300

    consertei minimax

--- src/BestMove.hs
+++ src/BestMove.hs
@@ -12,7 +12,7 @@ bestMove n b c
   | n < 0 = b
   | otherwise = bs !! (fromJust $ elemIndex (maximum mml) mml)
   where bs = possible_moves b c
-        mml = [miniMax n True b' c | b' <- bs]
+        mml = [miniMax (n-1) True b' c | b' <- bs]
 
 evalB b c | c == White = evaluateBoard b
           | c == Black = -(evaluateBoard b)
@@ -25,11 +25,11 @@ miniMax 0 _ b c = evalB b c
 miniMax n True b c = maximum list' 
   where newMoves = possible_moves b c
         mm try = miniMax (n-1) False try (other c)
-        list' = [if ebc > mm try then ebc else mm try | try <- newMoves]
+        list' = [mm try | try <- newMoves]
         ebc = evalB b c
  
 miniMax n False b c = minimum list'
   where newMoves = possible_moves b c
         mm try = miniMax (n-1) True try (other c)
-        list' = [if ebc < mm try then ebc else mm try | try <- newMoves]
+        list' = [mm try | try <- newMoves]
         ebc = evalB b c
