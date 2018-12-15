3b137c01dfa0459ee5d518bca94cb7fea9bd63c1
Pedro Queiroga <psq@cin.ufpe.br>
2018-12-14 23:02:01 -0300

    agora com alpha beta pruning

--- src/BestMove.hs
+++ src/BestMove.hs
@@ -12,24 +12,34 @@ bestMove n b c
   | n < 0 = b
   | otherwise = bs !! (fromJust $ elemIndex (maximum mml) mml)
   where bs = possible_moves b c
-        mml = [miniMax c (n-1) False b' (other c) | b' <- bs]
+        mml = [miniMax c (n-1) False b' (other c) (-99999999) 99999999 | b' <- bs]
 
 evalB b c | c == White = evaluateBoard b
           | c == Black = -(evaluateBoard b)
 
-miniMax :: Color -> Int -> Bool -> Board -> Color -> Int
-miniMax trueC 0 _ b c = evalB b trueC
+miniMax :: Color -> Int -> Bool -> Board -> Color -> Int -> Int -> Int
+miniMax trueC 0 _ b c _ _ = evalB b trueC
 
 -- cada elemento de list' tem um ramo. o elemento de maior(ou menor) eval eh o
 -- RAMO que devo escolher
-miniMax trueC n True b c = case newMoves of [] -> evalB b trueC
-                                            l -> maximum list'
+miniMax trueC n True b c alpha beta = case newMoves of [] -> evalB b trueC
+                                                       l -> forEach (-99999999) alpha beta newMoves
   where newMoves = possible_moves b c
-        mm try = miniMax trueC (n-1) False try (other c)
+        mm try alpha' beta' = miniMax trueC (n-1) False try (other c) alpha' beta'
         list' = [mm try | try <- newMoves]
+        forEach :: Int -> Int -> Int -> [Board] -> Int
+        forEach value alpha' beta (x:xs) = if maxav >= beta then maxvmm else forEach maxvmm maxav beta xs
+          where maxvmm = max value (mm x alpha' beta)
+                maxav  = max alpha' maxvmm
+        forEach value alpha' beta [] = value
  
-miniMax trueC n False b c = case newMoves of [] -> evalB b trueC
-                                             l -> minimum list'
+miniMax trueC n False b c alpha beta = case newMoves of [] -> evalB b trueC
+                                                        l -> forEach (99999999) alpha beta newMoves
   where newMoves = possible_moves b c
-        mm try = miniMax trueC (n-1) True try (other c)
+        mm try alpha' beta' = miniMax trueC (n-1) True try (other c) alpha' beta'
         list' = [mm try | try <- newMoves]
+        forEach :: Int -> Int -> Int -> [Board] -> Int
+        forEach value alpha beta' (x:xs) = if alpha >= minbv then minvmm else forEach minvmm alpha minbv xs
+          where minvmm = min value (mm x alpha beta')
+                minbv  = min beta minvmm
+        forEach value alpha beta' [] = value
